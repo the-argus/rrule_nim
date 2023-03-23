cea868b945e18ff6d7a8e91963da8ce2b8a6eb7d
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-03-23 09:13:16 -0400

    remove iter and between, back to just trying to implement fromText constructor

--- src/rrule.nim
+++ src/rrule.nim
@@ -1,9 +1,4 @@
-import std/times
-import std/options
-import sugar
-
 import private/r_options
-import private/iter
 
 type
   RRule* = object
@@ -15,11 +10,3 @@ proc freqIsDailyOrGreater*(freq: Frequency): bool =
 
 proc initRRule*(): RRule =
   RRule(origOptions: defaultOptions)
-
-# TODO: implement this
-proc between*(
-  self: RRule, after: DateTime, before: DateTime, inclusive: bool = false,
-  lambda: Option[(DateTime, number) -> bool] = none((DateTime, number) -> bool)
-  ): seq[DateTime] =
-  if lambda.isSome:
-    iter(self.origOptions)
