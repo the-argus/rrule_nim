30e4c12033f0f9772e77453f7de1313856220118
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-03-30 14:32:02 -0400

    implement on() function

--- src/rrule/private/r_options.nim
+++ src/rrule/private/r_options.nim
@@ -81,6 +81,10 @@ let
 
 proc at(ttr: var Parser, options: var Options)
 proc f(ttr: var Parser, options: var Options)
+proc on(ttr: var Parser, options: var Options)
+proc decodeM(ttr: var Parser): Option[number]
+proc decodeNTH(ttr: var Parser): Option[number]
+proc decodeWKD(ttr: var Parser): Option[string]
 
 proc fromText*(staticType: typedesc[Options], text: string): Option[Options] =
   var
@@ -109,9 +113,15 @@ proc fromText*(staticType: typedesc[Options], text: string): Option[Options] =
         at(ttr, options)
         f(ttr, options)
     of "weekday(s)":
-      discard
+      options.freq = WEEKLY
+      options.byweekday = @[dMon, dTue, dWed, dThu, dFri].some
+      ttr.nextSymbol()
+      f(ttr, options)
     of "week(s)":
-      discard
+      options.freq = WEEKLY
+      if ttr.nextSymbol():
+        on(ttr, options)
+        f(ttr, options)
     of "hour(s)":
       discard
     of "minute(s)":
@@ -152,6 +162,8 @@ proc at(ttr: var Parser, options: var Options) =
       # set options.byhour to an actual seq
       options.byhour.get().add(parseInt(matchBefore).number)
 
+    # TODO: ensure that this doesnt cause problems. copied from JS code, and its
+    # possible that JS runs both functions before checking their outputs
     let separatorSymbolIsNext = ttr.accept("comma") or ttr.accept("at")
 
     if not separatorSymbolIsNext:
@@ -170,6 +182,146 @@ proc f(ttr: var Parser, options: var Options) =
     options.count = parseInt(ttr.someValueFirstMatch).number.some
     ttr.expect("number")
 
+proc on(ttr: var Parser, options: var Options) =
+  let
+    hasOn = ttr.accept("on")
+    hasThe = ttr.accept("the")
+
+  if not (hasOn or hasThe): return
+
+  while true:
+    let
+      nth = ttr.decodeNTH
+      wkd = ttr.decodeWKD
+      m = ttr.decodeM
+
+    if nth.isSome:
+      if wkd.isSome:
+        discard ttr.nextSymbol()
+        if options.byweekday.isNone:
+          options.byweekday = some(seq[WeekDay](@[]))
+        var initial = options.byweekday.get()
+        initial.add()
+      else:
+        if options.bymonthday.isNone:
+          options.bymonthday = some(seq[number](@[]))
+        var initial = options.bymonthday.get()
+        initial.add(nth.get())
+        options.bymonthday = initial.some
+        discard ttr.accept("day(s)")
+    elif wkd.isSome:
+      discard ttr.nextSymbol()
+      if options.byweekday.isNone:
+        options.byweekday = some(seq[WeekDay](@[]))
+      var initial = options.byweekday.get()
+      initial.add()
+      options.byweekday = initial.some
+    elif ttr.someSymbol == "weekday(s)":
+      discard ttr.nextSymbol()
+      if options.byweekday.isNone:
+        options.byweekday = @[dMon, dTue, dWed, dThu, dFri].some
+    elif ttr.someSymbol == "week(s)":
+      discard ttr.nextSymbol()
+
+      let
+        v = ttr.someValueFirstMatch
+        num = ttr.acceptNumber()
+
+      if not num:
+        raise newException(Exception, "Unexpected symbol " & ttr.someSymbol & "; expected monthday.")
+
+      options.byweekno.get().add(parseInt(v).number)
+    elif m.isSome:
+      discard ttr.nextSymbol
+      if options.bymonth.isNone:
+        options.bymonth = some(seq[number](@[]))
+      var initial = options.bymonth.get()
+      initial.add(m.get())
+      options.bymonth = initial.some
+    else:
+      return
+    
+    # TODO: compare with how JS executes statements like this... would all get executed before evaluation? how does nim do it
+    let isSeparator = ttr.accept("comma") or ttr.accept("the") or ttr.accept("on")
+
+    if not isSeparator:
+      break
+
+
+proc decodeM(ttr: var Parser): Option[number] =
+  case ttr.someSymbol:
+    of "january":
+      result = 1.number.some
+    of "february":
+      result = 2.number.some
+    of "march":
+      result = 3.number.some
+    of "april":
+      result = 4.number.some
+    of "may":
+      result = 5.number.some
+    of "june":
+      result = 6.number.some
+    of "july":
+      result = 7.number.some
+    of "august":
+      result = 8.number.some
+    of "september":
+      result = 9.number.some
+    of "october":
+      result = 10.number.some
+    of "november":
+      result = 11.number.some
+    of "december":
+      result = 12.number.some
+    else:
+      result = number.none
+
+proc decodeWKD(ttr: var Parser): Option[string] =
+  let sym = ttr.someSymbol
+
+  const days: array[7, string] = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+
+  if sym in days:
+    return sym[0..2].toUpperAscii.some
+  else:
+    return string.none
+
+# may err: includes parseInt and requires the parsed
+# symbols to be within a range
+proc decodeNTH(ttr: var Parser): Option[number] =
+  case ttr.someSymbol:
+    of "last":
+      discard ttr.nextSymbol()
+      result = -1.number.some
+    of "first":
+      discard ttr.nextSymbol()
+      result = 1.number.some
+    of "second":
+      discard ttr.nextSymbol()
+      if ttr.accept("last"):
+        result = -2.number.some
+      else:
+        result = 2.number.some
+    of "third":
+      discard ttr.nextSymbol()
+      if ttr.accept("last"):
+        result = -3.number.some
+      else:
+        result = 3.number.some
+    of "nth":
+      let v = parseInt(ttr.someValueFirstMatch)
+      if v < -366 or v > 366:
+        raise newException(Exception, "Nth out of range: " & $v)
+
+      discard ttr.nextSymbol()
+
+      if ttr.accept("last"):
+        result = (-v).number.some
+      else:
+        result = v.number.some
+    else:
+      result = number.none
 
 # expose some fields read-only
 proc dtstart*(opt: ParsedOptions): DateTime = opt.dtstart
